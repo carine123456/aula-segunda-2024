3f8562840b5dcad3c706ca0d47fcaa8228ccd400
2028341 <cs119549@gmail.com>
2024-04-29 21:14:52 -0300

    AULA 29/04

--- app/src/main/java/com/example/aula2an/DatabaseActivity.java
+++ app/src/main/java/com/example/aula2an/DatabaseActivity.java
@@ -2,6 +2,7 @@ package com.example.aula2an;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.provider.ContactsContract;
 import android.view.View;
@@ -18,6 +19,7 @@ public class DatabaseActivity extends AppCompatActivity {
     private EditText editTextNome;
     private EditText editTextDatadeAdm;
     private EditText editTextSalario;
+    SimpleDateFormat dateFormat = new SimpleDateFormat("dd/mm/yyyy");
 
 
     @Override
@@ -44,7 +46,6 @@ public class DatabaseActivity extends AppCompatActivity {
         FuncionarioDao dao = db.funcionarioDao();
         int RE = Integer.parseInt(editTextRe.getText().toString());
         String nome = editTextNome.getText().toString();
-        SimpleDateFormat dateFormat = new SimpleDateFormat("dd/mm/yyyy");
         Date dataAdmissao;
         try {
             dataAdmissao = dateFormat.parse(editTextDatadeAdm.getText().toString());
@@ -53,7 +54,29 @@ public class DatabaseActivity extends AppCompatActivity {
         }
         double salario = Double.parseDouble(editTextSalario.getText().toString());
         Funcionario f = new Funcionario(RE, nome, dataAdmissao, salario);
-        dao.insert(f);
+        if (view.getId() == R.id.button3){
+            dao.insert(f);
+        } else if (view.getId() == R.id.button4){
+            dao.update(f);
+        } else {
+            dao.delete(f);
+        }
         limpar();
+    }
+
+    public void buscar (View view){
+        AppDatabase db = AppDatabase.getInstance(this);
+        FuncionarioDao dao = db.funcionarioDao();
+        int re = Integer.parseInt(editTextRe.getText().toString());
+        Funcionario f = dao.buscarPeloRe(re);
+        editTextNome.setText(f.getNome());
+        editTextDatadeAdm.setText(dateFormat.format(f.getDataAdmissao()));
+        editTextSalario.setText(Double.toString(f.getSalario()));
+
+    }
+
+    public void  lista(View view) {
+        Intent intent = new Intent(this, ListActivity.class);
+        startActivity(intent);
     }
 }
